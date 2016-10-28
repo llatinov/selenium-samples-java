ad3be8a0ecbd53f633744824b074c8f817d37718
Lyudmil Latinov <llatinov@gmail.com>
2016-10-28 18:18:56 +0300

    Add data driven unit test.

---
+++ junit/src/test/java/com/automationrhapsody/junit/LocatorDataDrivenTest.java
@@ -0,0 +1,68 @@
+package com.automationrhapsody.junit;
+
+import java.util.Arrays;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@RunWith(Parameterized.class)
+public class LocatorDataDrivenTest {
+
+    private static final Point MOCKED_POINT = new Point(11, 11);
+
+    private LocatorService locatorServiceMock = mock(LocatorService.class);
+
+    private Locator locatorUnderTest;
+
+    @Parameterized.Parameters(name
+        = "{index}: Test with X={0}, Y={1}, result is: {2}")
+    public static Iterable<Object[]> data() {
+        return Arrays.asList(new Object[][] {
+            {-1, -1, new Point(1, 1)},
+            {-1, 0, new Point(1, 0)},
+            {-1, 1, new Point(1, 1)},
+
+            {0, -1, new Point(0, 1)},
+            {0, 0, MOCKED_POINT},
+            {0, 1, MOCKED_POINT},
+
+            {1, -1, new Point(1, 1)},
+            {1, 0, MOCKED_POINT},
+            {1, 1, MOCKED_POINT}
+        });
+    }
+
+    private final int x;
+    private final int y;
+    private final Point expected;
+
+    public LocatorDataDrivenTest(int x, int y, Point expected) {
+        this.x = x;
+        this.y = y;
+        this.expected = expected;
+    }
+
+    @Before
+    public void setUp() {
+        when(locatorServiceMock.geoLocate(any(Point.class))).thenReturn(MOCKED_POINT);
+
+        locatorUnderTest = new Locator(locatorServiceMock);
+    }
+
+    @Test
+    public void testLocateResults() {
+        assertTrue(arePointsEqual(expected, locatorUnderTest.locate(x, y)));
+    }
+
+    private boolean arePointsEqual(Point p1, Point p2) {
+        return p1.getX() == p2.getX()
+            && p1.getY() == p2.getY();
+    }
+}
